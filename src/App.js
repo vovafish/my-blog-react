1c2286aef4d9de28db765a71ebf7f9d781a09bae
Vladimirs <vladimirribakov123@mail.ru>
2022-12-25 18:29:05 +0000

    Making navigation between link pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
-//import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>My Blog</h1>
+        <NavBar />
         <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />} />
