1a173f747d2a68e4e94ba174b78e98c6d7be9b44
Vladimirs <vladimirribakov123@mail.ru>
2022-12-25 17:49:29 +0000

    created basic routing and spacified all the basic pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+//import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <h1>This is the Not Found Page!</h1>;
+};
+
+export default NotFoundPage;
