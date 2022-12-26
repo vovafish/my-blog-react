a5f4e4d7c9853175283a4510b879962a2f18fc8e
Vladimirs <vladimirribakov123@mail.ru>
2022-12-26 21:11:55 +0000

    making the same origin for both back & front. imported react to use states

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticalInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticalInfo();
+  }, [articleId]);
+
   //const { articleId } = params;
   const article = articles.find((article) => article.name === articleId);
 
@@ -12,6 +25,7 @@ const ArticlePage = () => {
   ) : (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvotes</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
