46e0bc416727b2881952866b185a97b5616a041c
Vladimirs <vladimirribakov123@mail.ru>
2022-12-26 17:46:03 +0000

    adding logic for making comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: "learn-react",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "mongodb",
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -25,6 +28,20 @@ app.put("/api/articles/:name/upvote", (req, res) => {
   if (article) {
     article.upvotes += 1;
     res.send(`The ${name} article now has ${article.upvotes} upvotes!?`);
+  } else {
+    res.send("That article doesn't exist");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
   } else {
     res.send("That article doesn't exist");
   }
