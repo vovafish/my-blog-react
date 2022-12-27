40f7292be1930953e565dd86830aaec7987d2f3d
Vladimirs <vladimirribakov123@mail.ru>
2022-12-27 15:47:05 +0000

    connecting add from to the back-end

--- my-blog/src/components/AddCommentForm.js
+++ my-blog/src/components/AddCommentForm.js
@@ -1,3 +1,45 @@
-const AddCommentForm = () => {};
+import axios from "axios";
+import { useState } from "react";
+
+const AddCommentForm = ({ articleName, onArticleUpdated }) => {
+  const [name, setName] = useState("");
+  const [commentText, setCommentText] = useState("");
+
+  const addComment = async () => {
+    const response = axios.post(`/api/articles/${articleName}/comments`, {
+      postedBy: name,
+      text: commentText,
+    });
+    const updatedArticle = (await response).data;
+    onArticleUpdated(updatedArticle);
+    setName("");
+    setCommentText("");
+  };
+
+  return (
+    <div id="add-comment-form">
+      <h3>Add a Comment</h3>
+      <label>
+        Name:
+        {/* This is a two-way binding between input and value of the state*/}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Comment:
+        <textarea
+          row="4"
+          cols="50"
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+        />
+      </label>
+      <button onClick={addComment}>Add Comment</button>
+    </div>
+  );
+};
 
 export default AddCommentForm;
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -39,6 +40,10 @@ const ArticlePage = () => {
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
