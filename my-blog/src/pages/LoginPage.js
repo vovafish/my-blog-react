2f63154c6f21f2cf6c1b6d1745098188dab263a9
Vladimirs <vladimirribakov123@mail.ru>
2022-12-28 19:10:11 +0000

    building a login form

--- my-blog/src/pages/LoginPage.js
+++ my-blog/src/pages/LoginPage.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const LoginPage = () => {
-  return <h1>Login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate("/articles");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      {/* Display error if it actually exists */}
+      {error && <p className="error">{error}</p>}
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        placeholder="Your email address..."
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="You password..."
+      />
+      <button onClick={logIn}>Login</button>
+      <Link to="/create-account">Don't have an account? Create one here</Link>
+    </>
+  );
 };
 
 export default LoginPage;
